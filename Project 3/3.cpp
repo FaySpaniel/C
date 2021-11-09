007af100b74a43aa6f7c61113539a33c8cbee9f1
FaySpaniel <Fay.Spaniel@yandex.ru>
2021-11-09 12:46:26 +0300

    3

---
+++ Project 3/3.cpp
@@ -0,0 +1,44 @@
+﻿#include <iostream> 
+using namespace std;
+
+int main()
+{
+    setlocale(0, "");
+
+    int operation,
+        a,
+        b;
+    cout << "Выбери арифметическую операцию: \n[1] + \n[2] - \n[3] * \n[4] / \n[5] % \n";
+    cin >> operation;
+    cout << "Введи первое число: ";
+    cin >> a;
+    cout << "\nВведи второе число: ";
+    cin >> b;
+
+    if (operation == 1)
+    {
+        cout << "\n" << a << " + " << b << " = " << a + b;
+    }
+    else if (operation == 2)
+    {
+        cout << "\n" << a << " - " << b << " = " << a - b;
+    }
+    else if (operation == 3)
+    {
+        cout << "\n" << a << " * " << b << " = " << a * b;
+    }
+    else if (operation == 4)
+    {
+        cout << "\n" << a << " / " << b << " = " << a / b;
+    }
+    else if (operation == 5)
+    {
+        cout << "\n" << a << " % " << b << " = " << a % b;
+    }
+    else
+    {
+        cout << "\nШо error";
+    }
+
+    return 0;
+}
