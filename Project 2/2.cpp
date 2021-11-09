fba8d7a2a9920037dd556424de1fece40b0fd267
FaySpaniel <Fay.Spaniel@yandex.ru>
2021-11-09 12:49:38 +0300

    2

---
+++ Project 2/2.cpp
@@ -0,0 +1,33 @@
+﻿#include <iostream> 
+using namespace std;
+
+int main()
+{
+    setlocale(0, "");
+
+    int season;
+    cout << "Выбери время года: \n[1] Зима \n[2] Весна \n[3] Лето \n[4] Осень \n";
+    cin >> season;
+    if (season == 1)
+    {
+        cout << "Сейчас зима. Шапку надел";
+    }
+    else if (season == 2)
+    {
+        cout << "Сейчас весна. сапоги резиновые одел";
+    }
+    else if (season == 3)
+    {
+        cout << "Сейчас лето. шорты одел";
+    }
+    else if (season == 4)
+    {
+        cout << "Сейчас осень. одел портфель";
+    }
+    else
+    {
+        cout << "Нет такого времени года!";
+    }
+
+    return 0;
+}
